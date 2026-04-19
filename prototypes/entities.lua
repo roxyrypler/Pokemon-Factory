7a3d04c926c7e15a4bb14d43ec2abee27c5908b3
Patrick <patrick.lie9@gmail.com>
2026-04-19 13:19:17 +0200

    new art

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -5,6 +5,8 @@ local paper_press = table.deepcopy(data.raw["assembling-machine"]["assembling-ma
 local cardstock_press = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"])
 local pigment_extractor = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"])
 local disable_water_and_power = settings.startup["pokemon-factory-disable-water-and-power"].value
+local machine_collision_box_5x5 = {{-2.2, -2.2}, {2.2, 2.2}}
+local machine_selection_box_5x5 = {{-2.5, -2.5}, {2.5, 2.5}}
 
 local function make_free_powered(machine)
   if disable_water_and_power then
@@ -15,20 +17,28 @@ local function make_free_powered(machine)
   end
 end
 
-local function set_machine_art(machine, icon, entity_sprite)
+local function set_machine_art(machine, icon, entity_sprite, sprite_size, sprite_scale)
+  sprite_size = sprite_size or 320
+  sprite_scale = sprite_scale or 0.5
+
   machine.icon = icon
   machine.icon_size = 64
   machine.graphics_set = {
     animation = {
       filename = entity_sprite,
-      width = 128,
-      height = 128,
+      width = sprite_size,
+      height = sprite_size,
       frame_count = 1,
       shift = {0, 0},
-      scale = 0.75
+      scale = sprite_scale
     }
   }
   machine.graphics_set_flipped = nil
+end
+
+local function set_machine_footprint_5x5(machine)
+  machine.collision_box = machine_collision_box_5x5
+  machine.selection_box = machine_selection_box_5x5
 end
 
 shipping_box.name = "shipping-box"
@@ -53,6 +63,7 @@ set_machine_art(
   "__Pokemon-Factory__/graphics/icons/wood-chipper.png",
   "__Pokemon-Factory__/graphics/entities/wood-chipper.png"
 )
+set_machine_footprint_5x5(wood_chipper)
 wood_chipper.minable = {
   mining_time = 0.2,
   result = "wood-chipper"
@@ -69,6 +80,7 @@ set_machine_art(
   "__Pokemon-Factory__/graphics/icons/wood-pulper.png",
   "__Pokemon-Factory__/graphics/entities/wood-pulper.png"
 )
+set_machine_footprint_5x5(wood_pulper)
 wood_pulper.minable = {
   mining_time = 0.2,
   result = "wood-pulper"
@@ -77,6 +89,19 @@ wood_pulper.crafting_categories = {"pokemon-pulping"}
 wood_pulper.crafting_speed = 1
 wood_pulper.energy_usage = "120kW"
 wood_pulper.next_upgrade = nil
+wood_pulper.fluid_boxes = {
+  {
+    production_type = "input",
+    volume = 100,
+    pipe_connections = {
+      {
+        flow_direction = "input",
+        direction = defines.direction.north,
+        position = {0, -2}
+      }
+    }
+  }
+}
 make_free_powered(wood_pulper)
 
 paper_press.name = "paper-press"
@@ -85,6 +110,7 @@ set_machine_art(
   "__Pokemon-Factory__/graphics/icons/paper-press.png",
   "__Pokemon-Factory__/graphics/entities/paper-press.png"
 )
+set_machine_footprint_5x5(paper_press)
 paper_press.minable = {
   mining_time = 0.2,
   result = "paper-press"
@@ -99,8 +125,11 @@ cardstock_press.name = "cardstock-press"
 set_machine_art(
   cardstock_press,
   "__Pokemon-Factory__/graphics/icons/cardstock-press.png",
-  "__Pokemon-Factory__/graphics/entities/cardstock-press.png"
+  "__Pokemon-Factory__/graphics/entities/cardstock-press.png",
+  128,
+  1.25
 )
+set_machine_footprint_5x5(cardstock_press)
 cardstock_press.minable = {
   mining_time = 0.2,
   result = "cardstock-press"
@@ -117,6 +146,7 @@ set_machine_art(
   "__Pokemon-Factory__/graphics/icons/pigment-extractor.png",
   "__Pokemon-Factory__/graphics/entities/pigment-extractor.png"
 )
+set_machine_footprint_5x5(pigment_extractor)
 pigment_extractor.minable = {
   mining_time = 0.2,
   result = "pigment-extractor"
